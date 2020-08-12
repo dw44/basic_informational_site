2f6b86d5ffae3e6fb44d03a5b00fd1b7d991385f
Tanzeel ur Rehman <tanzeel.dw@gmail.com>
2020-08-13 01:30:34 +0500

    Views completed

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-const http = require('http');
+const http = require('http'),
+  url = require('url'),
+  fs = require('fs')
+  TEXT_HTML = {'Content-Type': 'text/html'};
+
 
 http.createServer((req, res) => {
+  console.log(url.parse(req.url.path, true));
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('The project has started!');
   res.end();
